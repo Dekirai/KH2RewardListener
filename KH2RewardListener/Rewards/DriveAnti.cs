90516ed294978ba35e4109acc2f282b1272cb46d
Dekirai <keivo1997@gmail.com>
2023-02-09 19:08:36 +0100

    Added several checks

--- KH2RewardListener/Rewards/DriveAnti.cs
+++ KH2RewardListener/Rewards/DriveAnti.cs
@@ -38,9 +38,9 @@ namespace KH2RewardListener.Rewards
                 {
                     int _isPaused = mem.ReadByte($"{process}.exe+AB9054");
                     int _isForm = mem.ReadByte($"{process}.exe+9AA5D4");
-                    int _canMove = mem.ReadByte($"{process}.exe+2A148E8");
+                    int _cantMove = mem.ReadByte($"{process}.exe+2A148E8");
                     int _isWorldMap = mem.ReadByte($"{process}.exe+714DB8");
-                    if (_isPaused > 0 || _isForm > 0 || _canMove > 0 || _isWorldMap == 15)
+                    if (_isPaused > 0 || _isForm > 0 || _cantMove > 0 || _isWorldMap == 15)
                     {
                         Thread.Sleep(3500);
                         continue;
